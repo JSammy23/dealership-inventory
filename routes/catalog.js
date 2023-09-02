2c29f7a3140b06adb7134193f2dd361ea97aca94
Jordan <jordansampson23@gmail.com>
2023-09-02 13:57:19 -0500

    Init catalog routes

---
+++ routes/catalog.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// Require controller modules
+const vehicleController = require('../controllers/vehicleController');
+
+// GET catalog home page
+router.get('/', vehicleController.index);
