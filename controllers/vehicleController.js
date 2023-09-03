a61fcd1a303923ed4ac5d8ac296cc33e2bb11230
Jordan <jordansampson23@gmail.com>
2023-09-03 11:07:40 -0500

    Init route functions.

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -5,3 +5,35 @@ const asyncHandler = require("express-async-handler");
 exports.index = asyncHandler(async (req, res, next) => {
     res.send("Not Implemented: Inventory Count")
 });
+
+exports.vehicle_create_get = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Create vehicle GET');
+});
+
+exports.vehicle_create_post = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Create vehicle POST');
+});
+
+exports.vehicle_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Delete vehicle GET');
+});
+
+exports.vehicle_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Delete vehicle POST');
+});
+
+exports.vehicle_update_get = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Update vehicle GET');
+});
+
+exports.vehicle_update_post = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Update vehicle POST');
+});
+
+exports.vehicle_detail = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Vehicle detail: ' + req.params.id);
+});
+
+exports.vehicle_list = asyncHandler(async (req, res, next) => {
+    res.send('Not Implemented: Vehicle list');
+});
