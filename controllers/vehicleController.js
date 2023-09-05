4df0b1f0f37a0bfe8f28e31f796f0751c5cb1b6e
Jordan <jordansampson23@gmail.com>
2023-09-05 11:53:01 -0500

    Vehicle update functions.

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -130,12 +130,106 @@ exports.vehicle_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.vehicle_update_get = asyncHandler(async (req, res, next) => {
-    res.send('Not Implemented: Update vehicle GET');
+    const [vehicle, allMakes, allCategories] = await Promise.all([
+        Vehicle.findById(req.params.id).exec(),
+        Make.find({}).exec(),
+        Category.find({}).exec()
+    ]);
+
+    res.render('vehicle_form', {
+        title: "Update Vehicle",
+        vehicle: vehicle,
+        make_list: allMakes,
+        category_list: allCategories,
+        errors: []
+    });
 });
 
-exports.vehicle_update_post = asyncHandler(async (req, res, next) => {
-    res.send('Not Implemented: Update vehicle POST');
-});
+exports.vehicle_update_post = [
+    // Validate & sanitize fields
+    body('make', 'Make must be selected.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    body('category', 'Category must be selected.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    body('year', 'Year must be entered.')
+        .trim()
+        .isLength({ min: 4 })
+        .isNumeric()
+        .escape(),
+    body('model', "Model must be entered.")
+        .trim()
+        .isLength({ min: 4 })
+        .escape(),
+    body('vin')
+        .if(body('vin').exists({ checkFalsy: true }))
+        .trim()
+        .escape(),
+    body('milage', 'Milage must be entered.')
+        .trim()
+        .isLength({ min: 1 })
+        .isNumeric()
+        .escape(),
+    body('summary', 'Summary must not be empty.')
+        .trim()
+        .notEmpty()
+        .escape(),
+    body('price', 'Price must be entered.')
+        .trim()
+        .isNumeric()
+        .isLength({ min: 3 })
+        .escape(),
+    body('color', "Color must be entered.")
+        .trim()
+        .notEmpty()
+        .escape(),
+    body('condition', 'Condition must be selected.')
+        .trim()
+        .notEmpty()
+        .escape(),
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const vehicle = new Vehicle({
+            year: req.body.year,
+            make: req.body.make,
+            model: req.body.model,
+            category: req.body.category,
+            vin: req.body.vin,
+            condition: req.body.condition,
+            milage: req.body.milage,
+            summary: req.body.summary,
+            price: req.body.price,
+            color: req.body.color,
+            _id: req.params.id // Required or a new id would be assigned.
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors, render form again with sanitized values & errors.
+            // Grab makes and categories
+            const [allMakes, allCategories] = await Promise.all([
+                Make.find({}).exec(),
+                Category.find({}).exec()
+            ]);
+
+            res.render('vehicle_form', {
+                title: 'Create New Vehicle',
+                make_list: allMakes,
+                category_list: allCategories,
+                vehicle: vehicle,
+                errors: errors.array()
+            });
+        } else {
+            const updatedVehicle = await Vehicle.findByIdAndUpdate(req.params.id, vehicle, {});
+            res.redirect(updatedVehicle.url);
+        }
+    })
+        
+];
 
 exports.vehicle_detail = asyncHandler(async (req, res, next) => {
     const vehicle = await Vehicle.findById(req.params.id).populate('make category').exec();
